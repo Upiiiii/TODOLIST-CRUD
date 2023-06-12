cce0c60838b72b4104e3ec2a94c85e3edd54936f
Upiiiii <taudah7890@gmail.com>
2023-06-12 10:57:32 +0700

    first commit

---
+++ db.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$host_name = 'localhost';
+$db_user = 'rafielut_luthfi';
+$db_pass = 'lu7hf1212412';
+$db_name = 'rafielut_luthfi';
+
+$con = mysqli_connect($host_name, $db_user, $db_pass, $db_name);
+
+?>
